5a4cd21221172ab04662655749989df14a6e2980
jdramirez <josedaniel8505@gmail.com>
2016-01-29 00:58:21 -0500

    Project Structure

---
+++ src/visualization.R
@@ -0,0 +1,36 @@
+#############################
+# 
+# STATW4701 
+# Homework 01 
+# 
+#
+# 
+#############################
+
+#################
+# Setup
+#################
+
+# make sure R is in the proper working directory
+# note that this will be a different path for every machine
+# setwd("")
+
+# first include the relevant libraries
+# note that a loading error might mean that you have to
+# install the package into your R distribution.
+# Use the package installer and be sure to install all dependencies
+library(ggplot2)
+
+#################
+# Clean Dataset
+#################
+
+##########################################
+# This code uses to clean 
+
+##########################################
+
+#################
+# End of Script
+#################
+
